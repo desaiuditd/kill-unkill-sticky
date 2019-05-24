4ab7879a15d09f5fbb5cc5fe00d7531ea91acef5
Udit Desai <desaiuditd@users.noreply.github.com>
2019-05-24 15:52:02 +1000

    Update kill-sticky.js

--- kill-sticky.js
+++ kill-sticky.js
@@ -1,20 +1,17 @@
 /**
  * This is a Chrome Bookmarklet.
  * Copy the code below and add it into the URL field of the Bookmark in Chrome.
+ *
+ * Dashes are converted to camelCase. dataset.camelCase in JS becomes data-camel-case in DOM.
+ * Ref: https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
  */
 javascript: ( () => {
 	const elements = document.querySelectorAll( 'body *' );
 	elements.forEach( element => {
-		// Bail, if it's not sticky.
 		if ( getComputedStyle( element ).position !== 'fixed' ) {
 			return;
 		}
-
 		element.style.visibility = 'hidden';
-		/*
-		 * Dashes are converted to camelCase. dataset.camelCase in JS becomes data-camel-case in DOM.
-		 * Ref: https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
-		 */
 		element.dataset.killUnkillStickyIsHidden = 'true'
 	} );
 } )();
